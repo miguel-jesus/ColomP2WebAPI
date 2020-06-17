f0e6cdb08176104b7a184d7928478a129371ae1f
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-06-17 20:13:27 +0200

    model y controller games

---
+++ ColomP2WebAPI/Controllers/GamesController.cs
@@ -0,0 +1,58 @@
+﻿using ColomP2WebAPI.Models;
+using Dapper;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ColomP2WebAPI.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Games")]
+    public class GamesController : ApiController
+    {
+        [HttpGet]
+        [Route("GetGamesInfo")]
+
+        public List<GamesModel> GetGamesInfo()
+        {
+            string authenticatedAspNetUserId = RequestContext.Principal.Identity.GetUserId();
+            using (IDbConnection cnn = new ApplicationDbContext().Database.Connection)
+            {
+                string sql = $"SELECT PlayerName,Age,HourGame,Score FROM dbo.Games ORDER BY Score ASC";
+
+                var games = cnn.Query<GamesModel>(sql).ToList();
+
+                return games;
+            }
+        }
+
+
+        [HttpPost]
+        [Route("InsertGame")]
+        public bool InsertGame(GamesModel games)
+        {
+            using (IDbConnection cnn = new ApplicationDbContext().Database.Connection)
+            {
+                string sql = $"INSERT INTO dbo.Games(PlayerName,Age,HourGame,Score) VALUES('{games.PlayerName}','{games.Age}','{games.HourGame}','{games.Score}')";
+
+                int rows = cnn.Execute(sql);
+                if (rows != 1)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ ColomP2WebAPI/Models/GamesModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ColomP2WebAPI.Models
+{
+    public class GamesModel
+    {
+        public string PlayerName { get; set; }
+        public string Age { get; set; }
+        public string HourGame { get; set; }
+        public string Score { get; set; }
+
+    }
+}
